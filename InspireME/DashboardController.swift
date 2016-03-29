18fa737bec19795ccaee5923b977fdf255e448ba
Brad Siegel <blsiege@gmail.com>
2016-03-28 22:58:21 -0500

    add ability to provide a custom title view

--- InspireME/DashboardController.swift
+++ InspireME/DashboardController.swift
@@ -2,32 +2,8 @@
 //  DashboardController.swift
 //  InspireME
 //
-//  Created by Brad Siegel on 3/25/16.
+//  Created by Brad Siegel on 3/28/16.
 //  Copyright © 2016 Brad Siegel. All rights reserved.
 //
 
-import UIKit
-
-class DashboardController: UIViewController,
-                           RequiresViewModel,
-                           RequiresSeguePerformer {
-    
-    private var viewModel : DashboardViewModel?
-    private var seguePerformer: SeguePerformer?
-    
-    @IBAction private func login() {
-        seguePerformer?.navigateWithSegue("landingSeg", dataForSegue: nil)
-    }
-    
-    //MARK:- RequiresViewModel
-    func setViewModel(viewModel: ViewModel) {
-        if let viewModel = viewModel as? DashboardViewModel {
-            self.viewModel = viewModel
-        }
-    }
-    
-    //MARK:- RequiresSeguePerformer
-    func setSeguePerformer(performer: SeguePerformer) {
-        self.seguePerformer = performer
-    }
-}
+import Foundation
---
+++ InspireME/LoginController.swift
@@ -0,0 +1,33 @@
+//
+//  DashboardController.swift
+//  InspireME
+//
+//  Created by Brad Siegel on 3/25/16.
+//  Copyright © 2016 Brad Siegel. All rights reserved.
+//
+
+import UIKit
+
+class DashboardController: UIViewController,
+                           RequiresViewModel,
+                           RequiresSeguePerformer {
+    
+    private var viewModel : DashboardViewModel?
+    private var seguePerformer: SeguePerformer?
+    
+    @IBAction private func login() {
+        seguePerformer?.navigateWithSegue("landingSeg", dataForSegue: nil)
+    }
+    
+    //MARK:- RequiresViewModel
+    func setViewModel(viewModel: ViewModel) {
+        if let viewModel = viewModel as? DashboardViewModel {
+            self.viewModel = viewModel
+        }
+    }
+    
+    //MARK:- RequiresSeguePerformer
+    func setSeguePerformer(performer: SeguePerformer) {
+        self.seguePerformer = performer
+    }
+}
